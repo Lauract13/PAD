8b908aecfcd3159562319b9064119720743c907c
Batchata <lacasa03@ucm.es>
2018-05-15 18:37:03 +0200

    baaby la vida es un siclo

--- SFinder/app/src/main/java/sfinder/app/MainActivity.java
+++ SFinder/app/src/main/java/sfinder/app/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
