ef27d198c9fc378f58b8a29f44cad3797b5f130e
Batchata <lacasa03@ucm.es>
2018-06-22 15:48:32 +0200

    cambio de contraseña

--- SFinder/app/src/main/java/sfinder/app/ChangePasswordActivity.java
+++ SFinder/app/src/main/java/sfinder/app/ChangePasswordActivity.java
@@ -4,8 +4,27 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
+
+
+import android.support.annotation.NonNull;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class ChangePasswordActivity extends FragmentActivity {
 
+
+    private EditText oldpassET;
+    private EditText newPassET;
+    private EditText newPassConfirmET;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -15,6 +34,49 @@ public class ChangePasswordActivity extends FragmentActivity {
 
     public void editarInformacion(View view)
     {
+        final FirebaseUser user;
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        final String email = user.getEmail();
+
+        oldpassET = findViewById(R.id.editPass);
+        String oldpass = oldpassET.getText().toString();
+
+        newPassET = findViewById(R.id.editContr);
+        final String newPass = newPassET.getText().toString();
+
+        newPassConfirmET = findViewById(R.id.editContrConf);
+        String newPassConfirm = newPassConfirmET.getText().toString();
+
+        AuthCredential credential = EmailAuthProvider.getCredential(email,oldpass);
+
+        if(newPassConfirm.equals( newPass)) {
+            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        user.updatePassword(newPass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(ChangePasswordActivity.this,"Se ha producido un error",Toast.LENGTH_SHORT).show();
+
+                                }else {
+                                    Toast.makeText(ChangePasswordActivity.this,"La contraseña ha sido modificada",Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+                    }else {
+                        Toast.makeText(ChangePasswordActivity.this,"Se ha producido un error en la autenticación",Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }else {
+            Toast.makeText(ChangePasswordActivity.this,"La confirmación de la contraseña no cohincide",Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 }
